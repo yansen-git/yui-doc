52acbc2dcf9a71a986c5e187b7c55a5df6528fae
yansen <120699334@qq.com>
2022-01-17 12:37:49 +0800

    fix: 删除多余字段

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	base: '/yui/',
-	title: "Heaven UI - 经典前端 UI 框架-test",
+	title: "Heaven UI - 经典前端 UI 框架",
 	description: "Heaven UI是一套开源的 Web UI 解决方案,其内部采用的是自身经典的模块化规范,并遵循原生 HTML/CSS/JS 的开发方式,极易上手,拿来即用。其外在极简,却又不失饱满的内在,体积轻盈,是一款轻量、可靠、好用的 Vue 组件库",
 	themeConfig: {
 		search: false, //禁用默认的搜索框
